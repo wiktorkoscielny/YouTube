c4bb9eb2e1cdcd913faa8c8662a7ca3ac26215b5
Wiktor <wiktorkoscielny@gmail.com>
2023-05-21 20:47:37 +0100

    Initial commit, application set-up, init TS, Tailwind, install modules and dependencies

---
+++ README.md
@@ -0,0 +1,3 @@
+# This will be a YouTube app created with React and Typescript, Redux state management and styled with Tailwind.
+
+### This site is under construction.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  ); 
+}
