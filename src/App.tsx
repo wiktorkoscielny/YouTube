2c73f2071540781e63e60ac10f9b72ed77a894a6
Wiktor <wiktorkoscielny@gmail.com>
2023-05-22 08:35:07 +0100

    YT-2 - create routes, code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./youtube/routes/Homepage/Homepage";
+import Searchpage from "./youtube/routes/Searchpage/Searchpage";
+import Watchpage from "./youtube/routes/Watchpage/Watchpage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/search" element={<Searchpage />} />
+        <Route path="/watch/:id" element={<Watchpage />} />
       </Routes>
     </BrowserRouter>
   ); 
