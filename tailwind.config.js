385c7b9f76888c534f84edeeddc52197b8d57de0
Wiktor <wiktorkoscielny@gmail.com>
2023-05-28 08:54:59 +0100

    YT-5 - A part of creating responsive navbar styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,11 @@ module.exports = {
   mode: 'jit',
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'yt-spec-base-background': '#0f0f0f'
+      },
+    },
   },
   plugins: [],
 }
-
