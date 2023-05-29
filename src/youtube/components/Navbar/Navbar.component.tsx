e3cb8bab569e03fabcbff1c849ef92de378ac38b
Wiktor <wiktorkoscielny@gmail.com>
2023-05-29 09:59:59 +0100

    YT-5 - Create flex search input styles, - create hidden search input.

--- src/youtube/components/Navbar/Navbar.component.tsx
+++ src/youtube/components/Navbar/Navbar.component.tsx
@@ -5,7 +5,7 @@
 
 import { PureComponent } from "react";
 import PropTypes from "prop-types";
-import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineClose, AiOutlineArrowLeft } from "react-icons/ai";
 import { TiMicrophone } from "react-icons/ti";
 import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -21,7 +21,7 @@ export class NavbarComponent extends PureComponent {
   render() {
     return (
       <div className="flex flex-row items-center justify-between h-[56px] px-4 items-center bg-yt-spec-base-background opacity-95 sticky top-0 z-50">
-        <div className="flex gap-8 items-center text-2xl">
+        <div className="flex gap-8 items-center text-2xl min-w-[170px]">
           <div>
             <GiHamburgerMenu />
           </div>
@@ -32,8 +32,8 @@ export class NavbarComponent extends PureComponent {
             </div>
           </Link>
         </div>
-        <div className="flex items-center justify-center gap-5">
-          <div>
+        <div className="flex items-center min-w-0 justify-center gap-5 ml-[25px]">
+          <div className="min-w-0">
             <form
             // onSubmit={}
             >
@@ -44,19 +44,19 @@ export class NavbarComponent extends PureComponent {
                   </div>
                   <input
                     type="text"
-                    className="bg-yt-spec-base-background focus:outline-none border-none"
+                    className="bg-yt-spec-base-background w-full focus:outline-none border-none"
                     placeholder="Search"
                     // value={}
                     // onChange={}
                   />
 
                   <AiOutlineClose
-                    className=""
+                    className="min-w-[15px]"
                     // onClick={}
                   />
                 </div>
                 <button
-                  className="h-10 flex items-center justify-center bg-zinc-800 border border-[#303030]"
+                  className="h-10 w-[64px] flex items-center justify-center bg-zinc-800 border border-[#303030]"
                   style={{ borderRadius: "0 25px 25px 0" }}
                 >
                   <AiOutlineSearch className="text-xl" />
@@ -68,7 +68,48 @@ export class NavbarComponent extends PureComponent {
             <TiMicrophone />
           </div>
         </div>
-        <div className="flex gap-5 items-center text-xl">
+        {/* hidden field  */}
+        <div className="flex absolute left-0 z-[51] w-screen bg-yt-spec-base-background items-center min-w-0 justify-center gap-5">
+        <div className="text-xl bg-zinc-900 rounded-full min-w-0 pl-3 pr-2">
+            <AiOutlineArrowLeft />
+          </div>
+          <div className="min-w-0">
+            <form
+            // onSubmit={}
+            >
+              <div className="flex bg-yt-spec-base-background items-center h-10 px-4 pr-0 border border-[#303030] rounded-3xl">
+                <div className="flex gap-4 items-center pr-5">
+                  <div>
+                    <AiOutlineSearch className="text-xl" />
+                  </div>
+                  <input
+                    type="text"
+                    className="bg-yt-spec-base-background w-full focus:outline-none border-none"
+                    placeholder="Search"
+                    // value={}
+                    // onChange={}
+                  />
+
+                  <AiOutlineClose
+                    className="min-w-[15px]"
+                    // onClick={}
+                  />
+                </div>
+                <button
+                  className="h-10 w-[64px] flex items-center justify-center bg-zinc-800 border border-[#303030]"
+                  style={{ borderRadius: "0 25px 25px 0" }}
+                >
+                  <AiOutlineSearch className="text-xl" />
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="text-xl bg-zinc-900 rounded-full min-w-[20px] ml-2 mr-3">
+            <TiMicrophone />
+          </div>
+        </div>
+        {/* end of hidden */}
+        <div className="flex gap-5 items-center text-xl min-w-[76px]">
           <div className="relative">
             <BsBell />
             <span className="absolute bottom-2 left-2 text-xs bg-red-600 rounded-full px-1">
