a3e886b670019f213a2698beb07ee559c44f9f0d
Wiktor <wiktorkoscielny@gmail.com>
2023-05-29 18:33:54 +0100

    YT-6 - Setup data api, start setting-up redux

--- src/youtube/store/index.ts
+++ src/youtube/store/index.ts
@@ -1,10 +1,25 @@
 import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
 import { InitialState } from "./types";
 
-const initialState: InitialState = {};
+const initialState: InitialState = {
+  videos: [],
+  currentPlayingVideo: null,
+  searchParams: "",
+  nextPageToken: null,
+  recommendedVideos: [],
+};
+
+const YouTubeSlice = createSlice({
+  name: "youtubeApp",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
 
 export const store = configureStore({
-    reducer: {}
+  reducer: {
+    youtubeApp: YouTubeSlice.reducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/youtube/store/types.ts
+++ src/youtube/store/types.ts
@@ -1,1 +1,11 @@
-export interface InitialState {};
+export interface InitialState {
+  videos: HomePageVideos[];
+  currentPlayingVideo: currentPlayingVideo | null;
+  searchParams: string;
+  nextPageToken: string | null;
+  recommendedVideos: RecommendedVideos[];
+}
+
+export interface HomePageVideos {}
+export interface currentPlayingVideo {}
+export interface RecommendedVideos {}
