30747cfa215571c623b0ae4fac316862535c7724
Wiktor <wiktorkoscielny@gmail.com>
2023-05-30 19:14:03 +0100

    YT-6 - part of store creation, create reducer

---
+++ src/youtube/store/reducers/getMainVideos.ts
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { RootState } from "..";
+import { YOUTUBE_API_URL } from "../../utils/constants";
+
+const API_KEY = process.env.REACT_APP_YOTUBE_DATA_API_KEY;
+
+export const getHomePageVideos = createAsyncThunk(
+  "youtubeApp/mainVideos",
+  async (isNext: boolean, { getState }) => {
+    const {
+      youtubeApp: { nextPageToken: nextPageTokenFromState, videos },
+    } = getState() as RootState;
+    const {
+      data: { items, nextPageToken },
+    } = await axios.get(
+      `${YOUTUBE_API_URL}/search?maxResults=20&q="hello world"&key=${API_KEY}&part=snippet&type=video&${
+        isNext ? `pageToken=${nextPageTokenFromState}` : ""
+      }`
+    );
+    console.log(items);
+  }
+);
