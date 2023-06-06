8803dcccd82bca6d3d1c5c361605dcb877bf0ae7
Wiktor <wiktorkoscielny@gmail.com>
2023-06-06 19:36:10 +0100

    YT-6 - Simplify/make more readable util. functions

--- src/youtube/utils/parseVideoDuration.ts
+++ src/youtube/utils/parseVideoDuration.ts
@@ -1,32 +1,47 @@
-export const parseVideoDuration = (duration: string): string => {
-  const durationParts: string[] = duration
-    .replace("PT", "")
-    .replace("H", ":")
-    .replace("M", ":")
-    .replace("S", "")
-    .split(":");
+export const parseVideoDuration = (duration: string): void => {
+  const durationSlices: string[] = duration
+  .replace("PT", "")
+  .replace("H", ":")
+  .replace("M", ":")
+  .replace("S", "")
+  .split(":");
 
-  return durationParts.length === 3
-    ? `${durationParts[0]}:${
-        parseInt(durationParts[1]) < 9
-          ? `0${durationParts[1]}`
-          : durationParts[1]
+  return _durationSwitcher(durationSlices);
+}
+
+const _durationSwitcher = (slices: string[]) => {
+  const slicesLength = slices.length;
+
+  switch (slicesLength) {
+    case 1:
+      `0:${
+        parseInt(slices[0]) < 9
+          ? `0${slices[0]}`
+          : slices[0]
+      }`;
+      break;
+
+    case 2:
+      `${slices[0]}:${
+        parseInt(slices[1]) < 9
+          ? `0${slices[1]}`
+          : slices[1]
+      }`;
+      break;
+
+    case 3:
+      `${slices[0]}:${
+        parseInt(slices[1]) < 9
+          ? `0${slices[1]}`
+          : slices[1]
       }:${
-        parseInt(durationParts[2]) < 9
-          ? `0${durationParts[2]}`
-          : durationParts[2]
+        parseInt(slices[2]) < 9
+          ? `0${slices[2]}`
+          : slices[2]
       }`
-    : durationParts.length === 2
-    ? `${durationParts[0]}:${
-        parseInt(durationParts[1]) < 9
-          ? `0${durationParts[1]}`
-          : durationParts[1]
-      }`
-    : durationParts.length === 1
-    ? `0:${
-        parseInt(durationParts[0]) < 9
-          ? `0${durationParts[0]}`
-          : durationParts[0]
-      }`
-    : "";
-};
+      break;
+
+    default:
+      "";
+  }
+}
