184de8c1e902bd2af4c45a4a9f92187ef1f685d1
Wiktor <wiktorkoscielny@gmail.com>
2023-07-01 18:39:08 +0100

    YT-10 - create a part of watchpage hoc, wrap watchpage in route provider, render content

---
+++ src/youtube/routes/Watchpage/Watchpage.container.tsx
@@ -0,0 +1,45 @@
+/*
+ * @category  Youtube
+ * @author    Wiktor Koscielny <wiktorkoscielny@gmail.com>
+ */
+
+import { PureComponent } from "react";
+import { connect } from "react-redux";
+import WatchpageComponent from "./Watchpage.component";
+
+/** @namespace Component/Watchpage/Container/mapStateToProps */
+function mapStateToProps(state: any) {
+  return {};
+}
+
+/** @namespace Component/Watchpage/Container/mapDispatchToProps */
+function mapDispatchToProps(dispatch: any) {
+  return {};
+}
+
+export type InheritedProps = {
+    navigation: any;
+    location: any;
+}
+
+/** @namespace Youtube/Component/Watchpage/Container */
+class WatchpageContainer extends PureComponent<InheritedProps> {
+
+  containerProps() {
+    const {
+        navigation,
+        location
+    } = this.props;
+
+    return {
+        navigation,
+        location
+    }
+  }
+
+  render() {
+    return <WatchpageComponent {...this.containerProps()} />;
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(WatchpageContainer);
