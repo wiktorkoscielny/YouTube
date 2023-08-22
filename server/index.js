86038195f9b003f2667be238ac9cfb049387681a
Wiktor <wiktorkoscielny@gmail.com>
2023-08-22 20:29:55 +0100

    YT-23 - clean files

--- server/index.js
+++ server/index.js
@@ -10,10 +10,6 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
-app.get("/api", (req, res) => {
-  res.json({ message: ` ${process.env.YOUTUBE_ENCODED}` });
-});
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
